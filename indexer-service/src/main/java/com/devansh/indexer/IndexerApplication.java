eed84d792ca053673fd8d55068b35ac80e5097ae
Devansh <guyawsm@gmail.com>
2026-03-23 20:43:56 +0530

    Adding basic services

---
+++ indexer-service/src/main/java/com/devansh/indexer/IndexerApplication.java
@@ -0,0 +1,13 @@
+package com.devansh.searchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchengineApplication.class, args);
+	}
+
+}
---
+++ pagerank-service/src/main/java/com/devansh/pagerank/PagerankApplication.java
@@ -0,0 +1,13 @@
+package com.devansh.searchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagerankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchengineApplication.class, args);
+	}
+
+}
